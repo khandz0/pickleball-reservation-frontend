8e3293d4661babcf43ce773d9cc2e008c5c52302
tkk2606 <kieuth24@gmail.com>
2025-03-26 21:50:20 -0700

    add app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+
+const App: React.FC = () => {
+  // This would typically come from your authentication system
+  const isAuthenticated = false;
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/home"
+          element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
